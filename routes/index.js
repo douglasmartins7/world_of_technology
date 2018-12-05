8fd8d7f3ac724948a9d0a081331a588e846d3ef6
Douglas <douglasmartinssantoss@gmail.com>
2018-12-05 17:00:59 -0200

    insert router cadastrar-pessoa

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var pessoas = [];
 
 /* GET home page. */
 //entra na rota 
-router.get('/', function(req, res, next) {
+router.get('/', function(request, response, next) {
   //acessa o arquivo index dentro da pasta view
   // passa os dados dinâmicos de title para o template ejs
-  res.render('index', { title: 'World of technology' });
+  response.render('index', { title: 'World of technology', pessoas: pessoas });
 });
 
-router.post('/cadastrar-pessoa', function(req, res, next) {
-   res.render('index', { title: 'cadastrar-pessoa' });
+router.post('/cadastrar-pessoa', function(request, response, next) {
+
+   var nome = request.body.nome;
+   hash = {
+      nome: request.body.nome,
+      sobrenome: request.body.sobrenome,
+      cpf: request.body.cpf,
+      telefone: request.body.telefone,
+      endereco: request.body.endereco
+   }
+
+   pessoas.push(hash);
+   response.render('index', { title: 'cadastrar-pessoa', pessoas: pessoas });
 });
 
 module.exports = router;
